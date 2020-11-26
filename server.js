4c2b7d4616d23663369b00035ed87de0deb29bb7
Akshaychavan41 <aschavan41@gmail.com>
2020-11-26 19:46:38 +0530

    heroku app fix

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/top-ramen'));
+app.use(express.static(__dirname + '/dist/topRamen'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/top-ramen/index.html'));
+res.sendFile(path.join(__dirname+'/dist/topRamen/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
